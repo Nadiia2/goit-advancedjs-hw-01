47eec62365cadc3b400aaeccd915cc0e4ecb3281
Nadiia2 <studenikina.nadya@gmail.com>
2024-04-15 21:37:06 +0200

    Homework 9

---
+++ src/js/2-form.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+
+const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (formData) {
+  email.value = formData.email || '';
+  message.value = formData.message || '';
+}
+
+form.addEventListener('input', function () {
+  const setData = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(setData));
+});
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const setData = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+
+  console.log(setData);
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+});
